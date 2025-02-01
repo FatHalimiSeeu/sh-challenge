2b9d5f377e5b0238ace60d73b74a73b2bfc921a3
FatHalimi <fat.halimi@i-solve.info>
2025-02-01 15:30:38 +0100

    Minor fix - Do not include Restock relation on response of all inventory items

--- models/inventory.go
+++ models/inventory.go
@@ -9,5 +9,5 @@ type InventoryItem struct {
 	Name        string    `gorm:"unique" json:"name" binding:"required" example:"Item A"`
 	Description string    `json:"description" example:"This is a sample item"`
 	Quantity    int       `json:"quantity" example:"100"`
-	Restocks    []Restock `gorm:"foreignKey:ItemID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" swaggerignore:"true"` // One-to-Many relationship, ignored in Swagger
+	Restocks    []Restock `gorm:"foreignKey:ItemID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-" swaggerignore:"true"` // One-to-Many relationship, ignored in Swagger
 }
